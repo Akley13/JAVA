256a0c3992792d9ed664019668c0104448e8da13
Akley Alves <akleyalvescr@gmail.com>
2023-10-04 17:40:13 -0300

    updt

---
+++ AprendendoJava/src/Variaveis.java
@@ -0,0 +1,23 @@
+public class Variaveis {
+    public static void main(String[] args) {
+
+        System.out.println("SCREEN MATCH");
+
+        System.out.println("Filme: Efeito borboleta");
+
+        int anoEfeitoBorboleta = 2004;
+        System.out.println("Ano de lançamento: " + anoEfeitoBorboleta);
+
+        double notaDoFilme = 9.8;
+        System.out.println("Nota: " + notaDoFilme);
+
+        boolean planoAssinatura = true;
+        System.out.println("Plano: " + planoAssinatura);
+
+        double mediaDoFilme = (9.8 + 7.0 + 8.0) / 3;
+        System.out.println("Média Geral: " + mediaDoFilme);
+
+        String sinopse = "Filme de suspense, estrelando com Asthon kutcher";
+        System.out.println(sinopse);
+    }
+}
